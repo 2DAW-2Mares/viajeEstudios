0e4786781bd8891ae9d6b18a8aaebf1632c9ba99
d8vjork <d8vjork@outlook.com>
2017-10-27 09:07:49 +0200

    Agregados datos a fixtures

---
+++ server/boot/migration.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = function(app) {
+    //data sources
+    var mysqlDs = app.dataSources.mysqlDs;
+
+    mysqlDs.automigrate(null, function (err) {
+        if (err) console.log(err);
+        console.log('> Models migrated to tables');
+        
+        app.loadFixtures()
+        .then(function() {
+          console.log('Done!');
+        })
+        .catch(function(err) {
+          console.log('Errors:', err);
+        });
+    });
+}
