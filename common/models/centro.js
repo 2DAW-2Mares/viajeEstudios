264c20f9498b95107343883e3fd0c3e86daac694
Alberto Sierra <alberto.sierra@murciaeduca.es>
2017-10-31 11:12:10 +0100

    Validaciones en el modelo de centros

--- common/models/centro.js
+++ common/models/centro.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = function(Centro) {
-
+	Centro.validatesNumericalityOf('codigocentro', {int: true, message: 'Debe ser un número sin decimales'});
+	Centro.validatesUniquenessOf('codigocentro', {message: 'Ese centro ya existe'});
 };
