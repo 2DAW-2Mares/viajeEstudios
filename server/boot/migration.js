d642c04c4722c7f08eb0775e07d57335c2c4d48e
Alberto Sierra <alberto.sierra@murciaeduca.es>
2017-10-31 11:13:12 +0100

    Utilizando variables de entorno para las credenciales de la base de datos y elegir cuando hacer el automigrate

--- server/boot/migration.js
+++ server/boot/migration.js
@@ -3,17 +3,18 @@
 module.exports = function(app) {
     //data sources
     var mysqlDs = app.dataSources.mysqlDs;
-
-    mysqlDs.automigrate(null, function (err) {
-        if (err) console.log(err);
-        console.log('> Models migrated to tables');
-        
-        app.loadFixtures()
-        .then(function() {
-          console.log('Done!');
-        })
-        .catch(function(err) {
-          console.log('Errors:', err);
+    if(process.env.AUTOMIGRATE) {
+        mysqlDs.automigrate(null, function (err) {
+            if (err) console.log(err);
+            console.log('> Models migrated to tables');
+            
+            app.loadFixtures()
+            .then(function() {
+              console.log('Done!');
+            })
+            .catch(function(err) {
+              console.log('Errors:', err);
+            });
         });
-    });
+    }
 }
---
+++ server/datasources.local.js
@@ -0,0 +1,15 @@
+// Copyright IBM Corp. 2014. All Rights Reserved.
+// Node module: loopback-example-offline-sync
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+module.exports = {
+  mysqlDs: {
+    connector: 'mysql',
+    hostname: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'viaje_estudios',
+  }
+};
