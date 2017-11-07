ef52e9309cbdc6aaf44e25b26a76fe3f58659b9f
Alberto Sierra <alberto.sierra@murciaeduca.es>
2017-11-07 10:27:56 +0100

    Enviando correo de confirmación

--- common/models/usuario.js
+++ common/models/usuario.js
@@ -1,5 +1,33 @@
 'use strict';
 
-module.exports = function(Usuario) {
+var path = require('path');
 
+module.exports = function(Usuario) {
+  //send verification email after registration
+  Usuario.afterRemote('create', function(context, user, next) {
+    var options = {
+      type: 'email',
+      to: user.email,
+      from: process.env.EMAIL_USER,
+      subject: 'Thanks for registering.',
+      template: path.resolve(__dirname, '../../server/views/verify.ejs'),
+      redirect: '/verified',
+      user: user
+    };
+
+    user.verify(options, function(err, response) {
+      if (err) {
+        User.destroyById(user.id);
+        return next(err);
+      }
+      context.res.render('response', {
+        title: 'Signed up successfully',
+        content: 'Please check your email and click on the verification link ' +
+            'before logging in.',
+        redirectTo: '/',
+        redirectToLinkText: 'Log in'
+      });
+    });
+  });
+  
 };
