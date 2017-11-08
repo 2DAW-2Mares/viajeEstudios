605a185490bbe55caa480fff54b93bea48fd7d93
Alberto Sierra <alberto.sierra@murciaeduca.es>
2017-11-08 11:06:31 +0100

    Posibilitando el login

--- server/boot/root.js
+++ server/boot/root.js
@@ -2,6 +2,7 @@
 var dsConfig = require('../datasources.local.js');
 
 module.exports = function(server) {
+  var User = server.models.Usuario;
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   //login page
@@ -16,6 +17,40 @@ module.exports = function(server) {
   //verified
   router.get('/verified', function(req, res) {
     res.render('verified');
+  });
+
+  //log a user in
+  router.post('/login', function(req, res) {
+    User.login({
+      email: req.body.email,
+      password: req.body.password
+    }, function(err, token) {
+      if (err) {
+        if(err.details && err.code === 'LOGIN_FAILED_EMAIL_NOT_VERIFIED'){
+          res.render('reponseToTriggerEmail', {
+            title: 'Login failed',
+            content: err,
+            redirectToEmail: '/api/Usuarios/'+ err.details.userId + '/verify',
+            redirectTo: '/',
+            redirectToLinkText: 'Click here',
+            userId: err.details.userId
+          });
+        } else {
+          res.render('response', {
+            title: 'Login failed. Wrong username or password',
+            content: err,
+            redirectTo: '/',
+            redirectToLinkText: 'Please login again',
+          });
+        }
+        return;
+      }
+      res.render('home', {
+        email: req.body.email,
+        accessToken: token.id,
+        redirectUrl: '/api/users/change-password?access_token=' + token.id
+      });
+    });
   });
 
   server.use(router);
